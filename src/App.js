b0ca9ab5223f189765256dd0b20bea8721439217
Joshua Rollo <joshuamrollo@gmail.com>
2022-01-05 04:07:05 -0600

    finished version1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import { FullPage, Slide } from 'react-full-page';
+import { nav, projects } from './items';
+import { Link } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <FullPage>
+      <navbar className='nav'>
+        {nav.map((item, idx) => {
+          return <Link className='nav-item hover-underline-animation' activeClass='active' to={item.path} spy={true} smooth={true} offset={0} duration={500} key={idx}>{item.name}</Link>
+        })}
+      </navbar>
+      <Slide>
+      <section className='page-1'>
+        <div className='container'>
+          <div className='page-1-content'>
+            <h10>Hey, my name is</h10>
+            <h1>Joshua Rollo.</h1>
+            <p className='page-1-p'>I am a software developer that is passionate about creating quality websites, applications, and everything in between.</p>
+          </div>
+        </div>
+      </section>
+      </Slide>
+      <Slide>
+      <section className='page-2'>
+        <div className='container'>
+          <div className='page-2-content'>
+            <h2>About Me</h2>
+            <p> 
+              Hello, my name is Joshua Rollo. After being introduced to programming in college I have fallen in love with it. As a soon to be graduate in May of 2022 I am now looking for employement as a software developer.
+            </p>
+            <p><br/>
+              Along with programming, I also love to ski and climb. Skiing is my favorite activity to do when on a break, I go whenever I get the opportunity. Rock climbing is my way of having fun while also being active, and with outdoor climbing you can get some amazing views.
+            </p>
+          </div>
+        </div>
+      </section>
+      </Slide>
+      <Slide>
+      <section className='page-3'>
+        <div className='container'>
+          <div className='page-3-content'>
+            <h2 className='project-title'>Projects</h2>
+            <div className='projects-wrapper'>
+              {projects.map((project, idx) => {
+                return (
+                  <div className={'project ' + project.class}  key={idx}>
+                    <h3 className='project-title'>{project.name}</h3>
+                    <img src={project.img} alt='gif of project' className='gif' />
+                    <a href={project.github}>Github</a>
+                    <a href={project.url}>Website</a>
+                    <p>{project.description}</p>
+                  </div>
+                )
+              })}
+              </div>
+          </div>
+        </div>
+      </section>
+      </Slide>
+      <Slide>
+      <section className='page-4'>
+        <div className='container'>
+          <div className='page-4-content'>
+            <h2 className='contact-title'>Contact</h2>
+              <div className='contacts'>
+                <a className='contact-item' href='https://github.com/Joshuamrollo'>
+                    <div className='item'><FontAwesomeIcon className='icon' icon={faGithub}></FontAwesomeIcon></div>
+                </a>
+                <a className='contact-item' href='https://www.linkedin.com/in/joshua-r-25b0b5102/'>
+                    <div className='item'><FontAwesomeIcon className='icon' href='#' icon={faLinkedin}></FontAwesomeIcon></div>
+                </a>
+                <a className='contact-item' href='mailto: joshuamrollo@gmail.com'>
+                    <div className='item'><FontAwesomeIcon className='icon' href='#' icon={faEnvelope}></FontAwesomeIcon></div>
+                </a>
+              </div>
+          </div>
+        </div>
+      </section>
+      </Slide>
+    </FullPage>
     </div>
   );
 }
---
+++ src/items.js
@@ -0,0 +1,45 @@
+export const nav = [
+    {
+      name: 'Home',
+      path: 'page-1'
+    },
+    {
+      name: 'About Me',
+      path: 'page-2'
+    },
+    {
+      name: 'Projects',
+      path: 'page-3'
+    },
+    {
+      name: 'Contact',
+      path: 'page-4'
+    }
+  ];
+
+export const projects = [
+    {
+      name: 'Sorting Visualizer',
+      description: 'A ReactJS project that visualizes arrays being sorted using popular sorting algorithms.',
+      github: 'https://github.com/Joshuamrollo/Sorting-Visualizer',
+      url: 'https://joshuamrollo.github.io/Sorting-Visualizer/',
+      class: 'sv',
+      img: 'https://media4.giphy.com/media/tAip7pLexcAASP4tSl/giphy.gif?cid=790b761181b6324c019c299b0de8f6aaeca31d2f9fbf6456&rid=giphy.gif&ct=g'
+    },
+    {
+      name: 'My Gaming List',
+      description: 'A fullstack project using NodeJS, ReactJS, and MongoDB where users can login, browse video games, rate them, and add them to their list.',
+      github: 'https://github.com/Joshuamrollo/my-gaming-list',
+      url: 'https://suspicious-bartik-d4bfcb.netlify.app/',
+      class: 'mgl',
+      img: 'https://media0.giphy.com/media/ny4mZiGLtyKyAonBCM/giphy.gif?cid=790b7611318c189654d3282ee2472b71795395a98fd33a17&rid=giphy.gif&ct=g'
+    },
+    {
+      name: 'Pathfinding Visualizer',
+      description: 'A ReactJS project that visualizes pathfinding using a grid and popular pathfinding algorithms.',
+      github: 'https://github.com/Joshuamrollo/Pathfinding-visualizer',
+      url: 'https://joshuamrollo.github.io/Pathfinding-visualizer/',
+      class: 'pv',
+      img: 'https://media1.giphy.com/media/EVMO0MOJTn1dG35EHP/giphy.gif?cid=790b76115d80b607ba11eb64c8413a0cf13af4dba1f2f79d&rid=giphy.gif&ct=g'
+    }
+  ]
